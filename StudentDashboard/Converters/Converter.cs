f24b7ab978f34a88d0791d6c1fdd22388fa80d1a
Surya <surya.elayaperumal@syncfusion.com>
2025-12-31 16:50:47 +0530

    removed unwanted namespace

--- StudentDashboard/Converters/Converter.cs
+++ StudentDashboard/Converters/Converter.cs
@@ -1,8 +1,6 @@
 ﻿using System.Globalization;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Media;
 
 namespace StudentPerformanceDashboard
 {
